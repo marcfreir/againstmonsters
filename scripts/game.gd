c360a936897ffce50c754664cb7b4774e515430f
marcfreir <marcfreir@outlook.com>
2023-02-26 14:47:13 -0200

    Update & set HUD with score

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,15 +1,22 @@
 extends Node
 
-
+var score = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	update_score()
 	get_node("monsterGroup").connect("monster_down", self, "on_monsterGroup_monster_down")
 
 
 func on_monsterGroup_monster_down(monster):
-	print(monster.score)
+	score += monster.score
+	update_score()
 
+#Make the score be written on the HUD
+func update_score():
+	get_node("HUD/scoreLabel").set_text(str(score))
+	
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
