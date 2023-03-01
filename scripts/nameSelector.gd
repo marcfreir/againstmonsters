4031cb21ece5ce28aea18f06180cdedc6e942c0c
marcfreir <marcfreir@outlook.com>
2023-02-28 22:52:05 -0200

    Update & make the name selector choose the player name

--- scripts/nameSelector.gd
+++ scripts/nameSelector.gd
@@ -1,14 +1,15 @@
 extends Node
 
-const characterList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789 *#"
+const characterList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789 *#@$%&!?"
 
 var index = 0
 var character = 0
 
+signal finished(playerName)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_process_input(true)
-
 
 func _input(event):
 	var indexChanged = false
@@ -22,8 +23,22 @@ func _input(event):
 		
 	
 	if indexChanged:
-		
+		if index < 0:
+			index = characterList.length() - 1
+		elif index >= characterList.length():
+			index = 0
 		get_node("nameSelectorHBoxContainer").get_child(character).set_text(characterList[index])
+	
+	if event.is_action_pressed("power_release") and not event.is_echo():
+		get_node("nameSelectorHBoxContainer").get_child(character).set_visible_characters(1)
+		index = 0
+		character += 1
+		if character > 2:
+			get_node("nameTimer").stop()
+			set_process_input(false)
+			var playerName = get_node("nameSelectorHBoxContainer").get_child(0).get_text() + get_node("nameSelectorHBoxContainer").get_child(1).get_text() + get_node("nameSelectorHBoxContainer").get_child(2).get_text()
+			emit_signal("finished", playerName)
+
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
