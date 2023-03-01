621cb10d57c3e7d4b43bf53c70028624ea5a4d78
marcfreir <marcfreir@outlook.com>
2023-03-01 13:21:54 -0200

    Update & find the higher score from the players

--- scripts/main.gd
+++ scripts/main.gd
@@ -4,6 +4,20 @@ extends Node
 var previousGame = preload("res://scenes/game.tscn")
 var game
 
+var highScoresList = [
+	{name = "AAA", score = 1000},
+	{name = "BBB", score = 900},
+	{name = "CCC", score = 800},
+	{name = "DDD", score = 700},
+	{name = "EEE", score = 600},
+	{name = "FFF", score = 500},
+	{name = "GGG", score = 400},
+	{name = "HHH", score = 300},
+	{name = "III", score = 200},
+	{name = "JJJ", score = 100}
+]
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -16,10 +30,10 @@ func new_game():
 	game = previousGame.instance()
 	
 	# Give me an error
-	#add_child(game)
+	add_child(game)
 	
 	# Replacement/Update
-	call_deferred("add_child", game)
+	#call_deferred("add_child", game)
 	
 	game.connect("game_over", self, "on_game_over")
 	game.connect("victory", self, "on_victory")
@@ -35,6 +49,12 @@ func _on_playButton_pressed():
 	new_game()
 
 func on_game_over():
+	
+	for highScore in highScoresList:
+		if game.gameData.score > highScore.score:
+			print(highScore)
+			break
+	
 	get_node("newGamePlayButton").show()
 
 func on_victory():
